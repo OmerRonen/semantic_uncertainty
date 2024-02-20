ae4f70736e4a43a6e72fd4c2b1671b4b77806b1e
OmerRonen <omerronen10@gmail.com>
2024-02-20 10:01:19 -0800

    nice

--- code/energy.py
+++ code/energy.py
@@ -55,7 +55,7 @@ def get_energy_logits(pre_softmax):
         factor_mul_log = 2 * torch.sum(torch.log(one_over_probs))
         c_term = torch.exp(-2 * log_c)
         energy = factor_mul_log + torch.log(1 + torch.sum((probs ** 2)) * c_term)
-        energies.append(energy / vocab_size)
+        energies.append(energy.detach().cpu().numpy() / vocab_size)
 
         if energy_total is None:
             energy_total = energy / vocab_size
