1e0188f4e3cd1af7dd0a6ab1cff58819c97845d2
OmerRonen <omerronen10@gmail.com>
2024-02-20 09:33:45 -0800

    nice

--- code/energy.py
+++ code/energy.py
@@ -47,7 +47,7 @@ def get_energy_logits(pre_softmax):
         token_pre_softmax = pre_softmax[:, i, :]
         token_pre_softmax = token_pre_softmax / temp
         probs = torch.softmax(token_pre_softmax, dim=-1)
-        print(probs)
+        print(probs.min())
 
         log_c = token_pre_softmax.logsumexp(dim=-1)
         one_over_probs = 1 / probs  # torch.clamp(1 / probs, min=1e-10, max=1e10)
