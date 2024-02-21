ba4e3ab27d1b595fcd35891a69bf7a6dd02628e3
OmerRonen <omerronen10@gmail.com>
2024-02-21 13:16:09 -0800

    nice

--- code/run_pipeline.sh
+++ code/run_pipeline.sh
@@ -11,19 +11,19 @@
 run_id=$1
 model=$2
 
-cd /accounts/campus/omer_ronen/projects/semantic_uncertainty/code
+cd /accounts/campus/omer_ronen/projects/semantic_uncertainty
 
 # run generation
-/accounts/campus/omer_ronen/.conda/envs/lso/bin/python generate.py --num_generations_per_prompt='5' --model=$model --fraction_of_data_to_use='0.1' --run_id=$run_id --temperature='0.5' --num_beams='1' --top_p='1.0'
+/accounts/campus/omer_ronen/.conda/envs/lso/bin/python code/generate.py --num_generations_per_prompt='5' --model=$model --fraction_of_data_to_use='0.1' --run_id=$run_id --temperature='0.5' --num_beams='1' --top_p='1.0'
 # get semantic similarities
-/accounts/campus/omer_ronen/.conda/envs/lso/bin/python clean_generated_strings.py  --generation_model=$model --run_id=$run_id
-/accounts/campus/omer_ronen/.conda/envs/lso/bin/python get_semantic_similarities.py --generation_model=$model --run_id=$run_id
+#/accounts/campus/omer_ronen/.conda/envs/lso/bin/python clean_generated_strings.py  --generation_model=$model --run_id=$run_id
+/accounts/campus/omer_ronen/.conda/envs/lso/bin/python code/get_semantic_similarities.py --generation_model=$model --run_id=$run_id
 # get likelihoods
-/accounts/campus/omer_ronen/.conda/envs/lso/bin/python get_likelihoods.py --evaluation_model=$model --generation_model=$model --run_id=$run_id
+/accounts/campus/omer_ronen/.conda/envs/lso/bin/python code/get_likelihoods.py --evaluation_model=$model --generation_model=$model --run_id=$run_id
 # get prompting based uncertainty
-/accounts/campus/omer_ronen/.conda/envs/lso/bin/python get_prompting_based_uncertainty.py --run_id_for_few_shot_prompt=$run_id --run_id_for_evaluation=$run_id
+/accounts/campus/omer_ronen/.conda/envs/lso/bin/python code/get_prompting_based_uncertainty.py --run_id_for_few_shot_prompt=$run_id --run_id_for_evaluation=$run_id
 # compute confidence measure
-/accounts/campus/omer_ronen/.conda/envs/lso/bin/python compute_confidence_measure.py --generation_model=$model --evaluation_model=$model --run_id=$run_id
+/accounts/campus/omer_ronen/.conda/envs/lso/bin/python code/compute_confidence_measure.py --generation_model=$model --evaluation_model=$model --run_id=$run_id
 
 
 
